143b079bf53ea39ef8961860dc2d0215e94f0a1a
Bertrand Janin <tamentis@neopulsar.org>
2011-03-20 19:26:28 -0400

    Update serve script to use the new location of app

--- tools/serve.py
+++ tools/serve.py
@@ -5,7 +5,7 @@ Debugging WSGI server.
 """
 
 from wsgiref.simple_server import make_server
-from vimpaste import app
+from vimpaste.app import app
 
 httpd = make_server('localhost', 9000, app)
 print("Serving vimpaste for debug on http://localhost:9000/.")
